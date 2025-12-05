89c1cccd3dbf6767fd2f1758cda776ff49f8b006
Pepancho <joscortes2021@udec.cl>
2025-12-05 14:18:21 -0300

    API update + index update
    Updated Api to fetch new information regarding patient, user and 'tasks' (this last one being unfinished). PatientCard.tsx and index.tsx have been updated to show real data. Tasks and events in index.tsx WONT WORK, as the api.ts file has to be updated with the corresponding function

--- components/home/PatientCard.tsx
+++ components/home/PatientCard.tsx
@@ -1,23 +1,54 @@
 import Colors from '@/constants/Colors';
 import { Image, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator } from 'react-native-paper';
 import StyledButton from '../ui/StyledButton';
 
-{/* Aqui simulamos traer los datos del paciente :P */}
+interface PatientProps {
+    patient: {
+        patient_id: number;
+        names: string;
+        surnames?: string;
+        care_group_id?: number;
+    } | null;
+    loading?: boolean;
+}
 
-export default function PatientCard() {
+export default function PatientCard({ patient, loading}: PatientProps) {
+
+    if (loading) {
+        return (
+            <View style={[styles.card, { justifyContent: 'center' }]}>
+                <ActivityIndicator color={Colors.white} />
+            </View>
+        )
+    }
+
+    if (!patient) {
+        return (
+            <View style={styles.card}>
+                <Text style={{color: Colors.white, fontFamily: 'Poppins-Regular'}}>
+                    No hay paciente seleccionado.
+                </Text>
+            </View>
+        )
+    }
+
     return (
         <View style={styles.card}>
-            <Image source={{ uri: 'https://i.pravatar.cc/100?u=ana' }} style={styles.avatar} />
+            <Image source={{ uri: 'https://i.pravatar.cc/150?img=11' }} style={styles.avatar} />
             <View style={styles.info}>
-                <Text style={styles.name}>Matías Medina</Text>
-                <Text style={styles.age}>82 Años</Text>
+                <Text style={styles.name}>{patient.names}</Text>
+                <Text style={styles.surname}>{patient.surnames || ''}</Text>
+                <Text style={styles.subtext}>Paciente Activo</Text>
+                {/*Añadiremos edad?*/}
+                {/*<Text style={styles.age}>82 Años</Text>*/}
             </View>
             <View style={styles.progressContainer}>
                 {/* Esto simula el círculo de progreso */}
                 <View style={styles.progressCircle}>
                     <Text style={styles.progressText}>85%</Text>
                 </View>
-                <StyledButton title="View Task" onPress={() => {}} style={{ paddingVertical: 8, marginTop: 5, backgroundColor: Colors.primaryDark }} />
+                <StyledButton title="Ver tareas" onPress={() => {}} style={{ paddingVertical: 8, marginTop: 5, backgroundColor: Colors.primaryDark }} />
             </View>
         </View>
     );
@@ -31,11 +62,13 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         marginVertical: 20,
-        
+        minHeight: 130,
     },
     avatar: { width: 90, height: 90, borderRadius: 35, borderWidth: 3, borderColor: Colors.white },
     info: { marginLeft: 15, flex: 1 },
     name: { color: Colors.white, fontSize: 25, fontWeight: 'bold',fontFamily: 'Poppins-Regular', },
+    surname: { color: Colors.white, fontSize: 18, fontFamily: 'Poppins-Regular', lineHeight: 20 },
+    subtext: { color: 'rgba(255,255,255,0.8)', fontSize: 14, fontFamily: 'Poppins-Regular', marginTop: 4 },
     age: { color: Colors.white, fontSize: 18, fontFamily: 'Poppins-Regular' },
     progressContainer: { alignItems: 'center' },
     progressCircle: {
@@ -47,6 +80,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         fontFamily: 'Poppins-Regular',
+        marginBottom: 5,
     },
     progressText: { color: Colors.white, fontWeight: 'bold', },
 });
