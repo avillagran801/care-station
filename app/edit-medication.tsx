837c5742585f30764cf5b455d53d203d6caba072
matiasmdlp <matiasmdlp95@gmail.com>
2025-12-08 22:33:11 -0300

    Fondo estándar
    Se agrego el fondo estándar a las vistas: contacts, documents, health, edit-medication, medications.
    Necesitamos proceder con el armado de estas vistas :/

--- app/edit-medication.tsx
+++ app/edit-medication.tsx
@@ -1,26 +1,34 @@
+import CustomSafeArea from '@/components/ui/CustomSafeArea';
 import ScreenHeader from '@/components/ui/ScreenHeader';
 import StyledButton from '@/components/ui/StyledButton';
 import Colors from '@/constants/Colors';
 import { useLocalSearchParams } from 'expo-router';
 import React from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text } from 'react-native';
+import { ImageBackground, ScrollView, StyleSheet, Text } from 'react-native';
 
 export default function EditMedicationScreen() {
   const { id } = useLocalSearchParams(); 
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <ScreenHeader title="Editar Medicamento" />
-      <ScrollView contentContainerStyle={styles.container}>
-        <Text>Editando medicamento con ID: {id}</Text>
-        <Text style={styles.placeholderText}>[Formulario de edición para el medicamento...]</Text>
-        <StyledButton title="Guardar cambios" onPress={() => {}} />
-        <StyledButton title="Eliminar" variant="secondary" onPress={() => {}} style={{ marginTop: 10, backgroundColor: '#fee2e2' }} />
-      </ScrollView>
-    </SafeAreaView>
+    <CustomSafeArea>
+      <ImageBackground
+              source={require('@/assets/images/background2.jpg')}
+              resizeMode="cover"
+              style={styles.backgroundImage}
+      >
+        <ScreenHeader title="Editar Medicamento" />
+        <ScrollView contentContainerStyle={styles.container}>
+          <Text>Editando medicamento con ID: {id}</Text>
+          <Text style={styles.placeholderText}>[Formulario de edición para el medicamento...]</Text>
+          <StyledButton title="Guardar cambios" onPress={() => {}} />
+          <StyledButton title="Eliminar" variant="secondary" onPress={() => {}} style={{ marginTop: 10, backgroundColor: '#fee2e2' }} />
+        </ScrollView>
+      </ImageBackground>
+    </CustomSafeArea>
   );
 }
 
 const styles = StyleSheet.create({
+  backgroundImage: { flex: 1, width: '100%'},
   safeArea: { flex: 1, backgroundColor: Colors.light.background },
   container: { padding: 20, gap: 15 },
   placeholderText: { textAlign: 'center', color: Colors.textLight, paddingVertical: 40 },
